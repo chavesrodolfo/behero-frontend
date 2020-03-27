4369ef878e95ed231b547c5356f3b1a9b31ea74c
Rodolfo <chavesrodolfo@gmail.com>
2020-03-27 18:38:03 -0300

    reset project

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Logon from './pages/Logon';
+import './global.css'
 
-function App() {
+export default function App() {
+   
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hey Hero!
-        </p>
-      </header>
-    </div>
+    <Logon/>
   );
 }
-
-export default App;
