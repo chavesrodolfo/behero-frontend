7c1a230b454333a4b1d052c30c076b906b91db7f
Rodolfo <chavesrodolfo@gmail.com>
2020-03-29 09:57:23 -0300

    layouts defined

--- src/pages/NewIncident/index.js
+++ src/pages/NewIncident/index.js
@@ -6,31 +6,26 @@ import { Link } from 'react-router-dom';
 
 export default function Register() {
   return (
-    <div className="register-container">
+    <div className="new-incident-container">
       <div className="content">
         <section className="form">
           <img src={logoImg} alt="Be The Hero" />
 
-          <h1>Cadastro</h1>
-          <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os acasos da sua ONG.</p>
+          <h1>Cadastrar novo caso</h1>
+          <p>Descreva o caso detalhadamente para encontrar um herói para resolver isso.</p>
 
-          <Link className="back-link" to="/">
+          <Link className="back-link" to="/profile">
             <FiArrowLeft size={16} color="#E02041" />
-              Já tenho Login
+              Voltar para Home
           </Link>
 
         </section>
 
         <form>
 
-          <input placeholder="Nome da ONG" />
-          <input type="email" placeholder="Email" />
-          <input placeholder="WhatsApp" />
-
-          <div className="input-group">
-            <input placeholder="Cidade" />
-            <input placeholder="UF" style={{ width: 80 }} />
-          </div>
+          <input placeholder="Título do caso" />
+          <textarea placeholder="Descrição" />
+          <input placeholder="Valor em reais" />
 
           <button className="button" type="submit">Cadastrar</button>
 
